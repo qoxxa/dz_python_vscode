db9c562c84b5222bc3c2d3c9909aeee1ed605c4e
qoxxa <lkostya.p@gmail.com>
2024-10-02 15:44:35 +0300

    Задание 1

---
+++ lesson_01/dz1.py
@@ -0,0 +1,26 @@
+my_height = 176
+print(my_height)
+
+my_name = "Konstantin"
+my_name = "Konstantin Pogodin"
+print(my_name)
+
+pet_name = input("Как зовут ващего питомца?")
+print("Вашего питомца зовут - "+pet_name)
+
+def print_python():
+    print("Учу Python!")
+print_python()
+
+def print_letter(let):
+    print(let, end='')
+print_letter('С')
+print_letter('т')
+print_letter('у')
+print_letter('д')
+print_letter('е')
+print_letter('н')
+print_letter('т')
+print_letter('t')
+print_letter('i')
+print_letter('n')
